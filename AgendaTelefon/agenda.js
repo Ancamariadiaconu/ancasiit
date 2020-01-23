218ccff8c7b295f1956282467cb1899a4cb69cce
Ancamariadiaconu <38043685+Ancamariadiaconu@users.noreply.github.com>
2020-01-23 10:04:49 -0800

    Agenda

---
+++ AgendaTelefon/agenda.js
@@ -0,0 +1,54 @@
+var list=[];
+var indexEdit; 
+function draw(){
+    var str="";
+    for(var i=0; i<list.length;i++){
+    var userInfo=list[i];
+        str+=`
+                 <tr>
+                     <td>${userInfo.name}</td>
+                     <td>${userInfo.telefon}</td>
+                     <td><button id="tableButtons" onclick="edit(${i})">Modifica</button></td>
+                     <td><button id="tableButtons" onclick="del(${i})");">Sterge</button></td>
+                  </tr>
+        
+        
+        
+        `;
+    }
+    document.querySelector("table tbody").innerHTML= str; 
+    
+}
+
+
+function add(event){
+ event.preventDefault();
+ var newContact={
+   name: document.querySelector("[name='contactName']").value,
+   telefon: document.querySelector("[name='telefonContact']").value
+
+ }
+ if(indexEdit===undefined){
+  list.push(newContact);
+
+ }
+ else{
+    list[indexEdit]=newContact;
+    window.indexEdit= undefined;
+ }
+  draw();
+  document.querySelector("div.displayContact").classList.remove("hidden");
+}
+function edit(idx){
+    document.querySelector("[name='contactName']").value=list[idx].name;
+    document.querySelector("[name='telefonContact']").value=list[idx].telefon;
+    window.indexEdit = idx;
+
+}
+function del(idx){
+    if(confirm("Esti sigur ca vrei sa stergi acest contact?")){
+        list.splice(idx,1);
+        draw();
+    }
+
+}
