0e2f23018fc35ee271e69ca369fd19f096ef4cfd
unknown <adiaconu41@yahoo.com>
2020-01-26 17:27:19 +0200

    listaCumparaturi

---
+++ listaCumparaturi/lista.js
@@ -0,0 +1,55 @@
+var list=[]; 
+function draw(){
+    var str="";
+    for(var i=0; i<list.length;i++){
+        str+=`
+            <tr>
+             <td>${list[i]}</td>
+             <td><button class="mark" onclick="edit(${i})">Mark as buyed</button></td>
+            </tr>
+        
+        
+        `;
+    }
+    document.querySelector("table tbody").innerHTML= str;    
+}
+function add(event){
+    event.preventDefault();
+    item=document.querySelector("[name='product']").value
+    if(item.length>0){
+     list.push(item);
+     draw();
+     document.querySelector("[name='product']").value=""; 
+     document.querySelector("div.displayItems").classList.remove("hidden");
+   }
+}
+function edit(idx){
+    
+    for(var i=list.length; i>=1;i--){
+        if(!document.querySelector("table tr:nth-child("+(i)+") td:nth-child("+1+")").classList.contains("cut")){
+            temp=list[i];
+            list[i]=list[idx+1];
+            list[idx+1]=temp; 
+            
+        }
+    }
+    
+    draw(); 
+    document.querySelector("table tr:nth-child("+(idx+1)+") td:nth-child("+1+")").classList.add("cut");
+}
+function sortAsc(event){
+    list.sort();
+      draw(); 
+}
+function sortDesc(event){
+  for (var i = 0; i < list.length; i++) {
+    for (var j = 0; j < list.length; j++) {
+        if (list[j] < list[j + 1]) {
+             tmp = list[j];
+             list[j] = list[j + 1];
+             list[j + 1] = tmp;
+        }
+    }
+}
+draw();
+}
