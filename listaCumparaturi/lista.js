b2fb94af3a58c272c76a41df517dc9827fff5cad
Ancamariadiaconu <38043685+Ancamariadiaconu@users.noreply.github.com>
2020-02-01 14:29:13 +0200

    Update lista.js

--- listaCumparaturi/lista.js
+++ listaCumparaturi/lista.js
@@ -27,13 +27,21 @@ function edit(idx){
     document.querySelector("table tr:nth-child("+(idx+1)+") td:nth-child("+1+")").classList.add("cut");
 }
 function sortAsc(event){
-    list.sort();
+    for (var i = 0; i < list.length; i++) {
+    for (var j = 0; j < list.length; j++) {
+        if (list[j].toLowerCase() > list[j + 1].toLowerCase(){
+             tmp = list[j];
+             list[j] = list[j + 1];
+             list[j + 1] = tmp;
+         }
+      }
+   }
       draw(); 
 }
 function sortDesc(event){
   for (var i = 0; i < list.length; i++) {
     for (var j = 0; j < list.length; j++) {
-        if (list[j] < list[j + 1]) {
+        if (list[j].toLowerCase() < list[j + 1].toLowerCase() {
              tmp = list[j];
              list[j] = list[j + 1];
              list[j + 1] = tmp;
